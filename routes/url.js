b8241924b4fe4e8f06a93ea6394e1fba6fabd06b
Kshitij Todkar <120639775+KshitijTodkar48@users.noreply.github.com>
2023-05-20 22:58:41 +0530

    Delete routes directory

--- routes/url.js
+++
@@ -1,9 +0,0 @@
-const express = require("express") ;
-const router = express.Router() ;
-const {handleGenerateNewShortUrl, handleGetAnalytics} = require("../contollers/url") ;
-
-router.post('/' , handleGenerateNewShortUrl) ;
-
-router.get('/analytics/:shortId' , handleGetAnalytics) ;
-
-module.exports = router ;
