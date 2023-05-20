99fdc83649c738b9f2db6890b70dd9da0d5d8516
Kshitij Todkar <120639775+KshitijTodkar48@users.noreply.github.com>
2023-05-20 23:03:24 +0530

    added roures

---
+++ routes/staticRouter.js
@@ -0,0 +1,28 @@
+const express = require("express") ;
+const URL = require("../models/url") ;
+const { restrictTo } = require("../middlewares/auth");
+const router = express.Router() ;
+
+router.get("/admin/urls", restrictTo(["ADMIN"]) , async (req , res) => {
+    const allUrls = await URL.find({}) ;
+    return res.render("home", {
+        urls: allUrls , 
+    }) ;
+});
+
+router.get("/", restrictTo(["NORMAL","ADMIN"]) , async (req , res) => {
+    const allUrls = await URL.find({createdBy: req.user.id}) ;
+    return res.render("home", {
+        urls: allUrls , 
+    }) ;
+});
+
+router.get("/signup", (req , res) => {
+    return res.render("signup") ;
+})
+
+router.get("/login", (req , res) => {
+    return res.render("login") ;
+})
+
+module.exports = router ;
