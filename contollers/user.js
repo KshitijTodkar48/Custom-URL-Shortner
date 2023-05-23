1c7f0b34a09042a8725acf6755d47477a6caae21
Kshitij Todkar <120639775+KshitijTodkar48@users.noreply.github.com>
2023-05-23 22:25:38 +0530

    Added controllers

---
+++ contollers/user.js
@@ -0,0 +1,36 @@
+const User = require("../models/user")
+const { setUser } = require("../service/auth")
+
+async function handleUserSignup(req , res) {
+    const { name, email, password } = req.body ;
+    await User.create({
+        name ,
+        email ,
+        password ,
+    }) ;
+    return res.redirect("/") ;
+}
+
+async function handleUserLogin(req , res) {
+    const { email, password } = req.body ;
+    const user = await User.findOne({
+        email ,
+        password ,
+    }) ;
+    if(!user)
+    {
+        return res.render("login", {
+            error: "Invalid username or password."
+        })
+    }
+
+    const token = setUser(user) ;
+    res.cookie("token" , token) ;
+
+    return res.redirect("/") ;
+}
+
+module.exports = {
+    handleUserSignup,
+    handleUserLogin,
+}
